1d55eb73a35af2c903fb711a4b95f3599fc3ac84
lMatkovic <lmatkovic00@gmail.com>
2024-05-24 23:01:49 +0200

    SQL

---
+++ SQLQuery1.sql
@@ -0,0 +1,39 @@
+﻿--ovo je linija koja se neće izvesti
+--create database edunovawp5;
+
+--drop database edunovawp5;
+
+--use edunovawp5;
+
+--create table smjerovi(
+--sifra int,
+--naziv varchar(50),
+--cijena decimal(18,2),
+--brojsati int,
+--izvodiseod datetime,
+--vaucer bit
+--);
+
+--create table grupe(
+--sifra int,
+--naziv varchar(20),
+--brojslobodnihmjesta int,
+--datumpocetka datetime,
+--smjer int,
+--predavac varchar(50)
+--);
+
+
+--create table polaznici(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--email varchar(100),
+--oib char(11)
+--);
+
+
+--create table clanovi(
+--grupa int,
+--polaznik int
+--);
