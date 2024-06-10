7d6136d43bb367192da5d1a504e50732ab365708
lMatkovic <lmatkovic00@gmail.com>
2024-06-11 01:41:47 +0200

    Update Zadaca1.sql

--- Zadace/Zadaca1.sql
+++ Zadace/Zadaca1.sql
@@ -51,3 +51,34 @@ alter table utakmica add foreign key (domaci_klub) references klub(sifra);
 alter table utakmica add foreign key (gostujuci_klub) references klub(sifra);
 alter table trener add foreign key (klub) references klub(sifra);
 
+insert into klub (naziv, osnovan, stadion, predsjednik, drzava, liga) values
+('FC Barcelona', 1899, 'Camp Nou', 'Joan Laporta', 'Španjolska', 'La Liga'),
+('Real Madrid', 1902, 'Santiago Bernabeu', 'Florentino Perez', 'Španjolska', 'La Liga'),
+('Manchester United', 1878, 'Old Trafford', 'Joel Glazer', 'Engleska', 'Premier League'),
+('Bayern Munich', 1900, 'Allianz Arena', 'Herbert Hainer', 'Njema?ka', 'Bundesliga');
+
+--select * from klub;
+
+insert into igrac (ime, prezime, datum_ro?enja, pozicija, broj_dresa, klub) values
+('Robert', 'Lewandowski', '1988-08-21', 'Napada?', 9, 1),
+('Luka', 'Modri?', '1985-09-09', 'Vezni', 10, 2),
+('Harry', 'Maguire', '1993-03-05', 'Obrambeni', 5, 3),
+('Manuel', 'Neuer', '1986-03-27', 'Golman', 1, 4); 
+
+--select * from igrac;
+
+insert into utakmica (datum, vrijeme, lokacija, stadion, domaci_klub, gostujuci_klub) values
+('2023-08-10', '20:00:00', 'Barcelona', 'Camp Nou', 1, 2),
+('2023-09-15', '21:00:00', 'Madrid', 'Santiago Bernabeu', 2, 3),
+('2024-03-21', '19:00:00', 'Manchester', 'Old Trafford', 3, 4),
+('2024-04-08', '18:00:00', 'Munich', 'Allianz Arena', 4, 1);
+
+--select * from utakmica;
+
+insert into trener (ime, prezime, klub, nacionalnost, iskustvo) values
+('Hansi', 'Flick', 1, 'Nijemac', '5 godina'),
+('Carlo', 'Ancelotti', 2, 'Talijan', '20 godina'),
+('Erik', 'ten Hag', 3, 'Nizozemac', '10 godina'),  
+('Vincent', 'Kompany', 4, 'Belgijac', '7 godina'); 
+
+--select * from trener;
