2f32660996caa5871eca3ce45fcc90c23153c82b
lMatkovic <lmatkovic00@gmail.com>
2024-06-10 23:38:47 +0200

    Dodavanje not null

--- Zadace/Zadaca1.sql
+++ Zadace/Zadaca1.sql
@@ -7,43 +7,43 @@ go
 use nogometniklub;
 
 
-CREATE TABLE klub(
-sifra INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-naziv VARCHAR(30) NOT NULL,
-osnovan INT,
-stadion VARCHAR(40),
-predsjednik VARCHAR(50),
-drzava VARCHAR(30),
-liga VARCHAR(40)
+create table klub(
+sifra int not null primary key identity(1,1),
+naziv varchar(30) not null,
+osnovan int,
+stadion varchar(40),
+predsjednik varchar(50),
+drzava varchar(30),
+liga varchar(40)
 );
 
-CREATE TABLE igrac(
-sifra INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-ime VARCHAR(30),
-prezime VARCHAR(30),
-datum_ro?enja DATETIME,
-pozicija VARCHAR(30),
-broj_dresa INT,
-klub INT
+create table igrac(
+sifra int not null primary key identity(1,1),
+ime varchar(30) not null,
+prezime varchar(30) not null,
+datum_ro?enja datetime not null,
+pozicija varchar(30),
+broj_dresa int,
+klub int not null
 );
 
-CREATE TABLE utakmica(
-sifra INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-datum DATETIME,
-vrijeme TIME,
-lokacija VARCHAR(30),
-stadion VARCHAR(30),
-domaci_klub INT,
-gostujuci_klub INT,
+create table utakmica(
+sifra int not null primary key identity(1,1),
+datum datetime not null,
+vrijeme time not null,
+lokacija varchar(30),
+stadion varchar(30),
+domaci_klub int not null,
+gostujuci_klub int not null,
 );
 
-CREATE TABLE trener(
-sifra INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-ime VARCHAR(30),
-prezime VARCHAR(30),
-klub INT,
-nacionalnost VARCHAR(30),
-iskustvo VARCHAR(30),
+create table trener(
+sifra int not null primary key identity(1,1),
+ime varchar(30) not null,
+prezime varchar(30) not null,
+klub int not null,
+nacionalnost varchar(30),
+iskustvo varchar(30),
 );
 
 alter table igrac add foreign key (klub) references klub(sifra);
