b7af5d52c97e4b74528397f99f9ef65babaf0432
lMatkovic <lmatkovic00@gmail.com>
2024-06-20 20:59:45 +0200

    Update Program.cs

--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Edunova!");
+Console.WriteLine("osijek");
+
+// ovo je komentar linija
+
+/*
+ 
+komentar kroz više linija
+
+*/
+
+Console.WriteLine("Zadnja linija čšćđćČĆŠĐŽ");
+
