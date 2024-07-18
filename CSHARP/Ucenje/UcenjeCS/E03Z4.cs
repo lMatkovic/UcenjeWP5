fa660490a4c8f73958be97a80faf00e71feb0dc5
lMatkovic <lmatkovic00@gmail.com>
2024-07-18 20:03:08 +0200

    Dz

--- CSHARP/Ucenje/UcenjeCS/E03Z4.cs
+++ CSHARP/Ucenje/UcenjeCS/E03Z4.cs
@@ -8,21 +8,34 @@ namespace UcenjeCS
 {
     internal class E03Z4
     {
-        /*
-         *  KORISTEĆI IF
-         * 
-        Program učitava od korisnika ime grada.U ovisnosti o imenu grada ispisuje regiju prema sljedećim pravilima:
 
-       Osijek -> Slavonija
-       Zadar -> Dalmacija
-       Čakovec -> Međimurje
-       Pula -> Istra
-       Za ostale unose program ispisuje: Ne znam koja je to regija.
-        */
-       
-        // dodatno: Odraditi istu logiku s switch
+        public static void Izvedi()
+        {
+            Console.WriteLine("Unesite ime grada:");
+            string grad = Console.ReadLine();
 
-
-        
+            if (grad == "Osijek")
+            {
+                Console.WriteLine("Slavonija");
+            }
+            else if (grad == "Zadar")
+            {
+                Console.WriteLine("Dalmacija");
+            }
+            else if (grad == "Čakovec")
+            {
+                Console.WriteLine("Međimurje");
+            }
+            else if (grad == "Pula")
+            {
+                Console.WriteLine("Istra");
+            }
+            else
+            {
+                Console.WriteLine("Ne znam koja je to regija.");
+            }
+        }
     }
 }
+
+
