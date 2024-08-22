b2219e0130e5f806641f334a2c4c16a924b9caea
lMatkovic <lmatkovic00@gmail.com>
2024-08-22 21:06:23 +0200

    Update Utakmica.cs

--- CSHARP/Ucenje/UcenjeCS/E16NasljedivanjePolimorfizam/ZavrsniRad/Utakmica.cs
+++ CSHARP/Ucenje/UcenjeCS/E16NasljedivanjePolimorfizam/ZavrsniRad/Utakmica.cs
@@ -11,6 +11,9 @@ namespace UcenjeCS.E16NasljedivanjePolimorfizam.ZavrsniRad
         public DateTime? Datum { get; set; }
         public string? Lokacija { get; set; }
         public string? Stadion { get; set; }
+        public Klub DomaciKlub { get; set; }
+        public Klub GostujuciKlub { get; set; }
+             
         
     }
 }
