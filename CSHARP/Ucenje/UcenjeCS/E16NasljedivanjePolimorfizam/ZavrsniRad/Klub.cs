c6c3cd9584d0c3df9f6bcdd78a0bcfdc500d405f
lMatkovic <lmatkovic00@gmail.com>
2024-09-03 17:54:57 +0200

    Update Klub.cs

--- CSHARP/Ucenje/UcenjeCS/E16NasljedivanjePolimorfizam/ZavrsniRad/Klub.cs
+++ CSHARP/Ucenje/UcenjeCS/E16NasljedivanjePolimorfizam/ZavrsniRad/Klub.cs
@@ -17,6 +17,24 @@ namespace UcenjeCS.E16NasljedivanjePolimorfizam.ZavrsniRad
         public string? Liga { get; set; }
 
 
+        public Klub(int entitet, string? naziv, DateTime osnovan, string? stadion, string? predsjednik, string? drzava, string? liga)
+        {
+
+                 Sifra = entitet;
+                 Naziv = naziv;
+                 Osnovan = osnovan;
+                 Stadion = stadion;
+                 Predsjednik = predsjednik;
+                 Drzava = drzava;
+                 Liga = liga;                             
+
+        }
+
+        public override string ToString()
+        {
+            return $"Klub: {Naziv}, Osnovan: {Osnovan}, Stadion: {Stadion}, Predsjednik: {Predsjednik}, Drzava: {Drzava}, Liga: {Liga}";
+        }
+
 
     }
 }
